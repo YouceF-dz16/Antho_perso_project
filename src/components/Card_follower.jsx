dd3c63f42d24a3a0c4f432cd68e6531fd4950473
Youcef Aissat <y.aissat@gmail.com>
2023-06-05 02:18:38 +0200

    update commit

--- src/components/Card_follower.jsx
+++ src/components/Card_follower.jsx
@@ -8,7 +8,7 @@ export default function Card_follower (props){
                 <img src={props.item.social} className="facebook--img"></img>
                 <span className="card--email">{props.item.email}</span>
             </div>
-            <p className='nb-followers'>{props.item.followers}</p>
+            <p className='nb-followers'>{props.item.follower}</p>
             <p className='p-followers'>{props.item.describe}</p>
             <div className='up-icon-text'>
                 <img src={props.item.icon?"src/assets/icon-up.svg":"src/assets/icon-down.svg"} alt="icon" className="up-icon-card"/>
